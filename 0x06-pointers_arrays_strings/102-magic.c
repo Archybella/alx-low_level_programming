0b88a06a20d9a8ece0d38b9931e9f62663cbabc3
Archybella <berylevelyn96@gmail.com>
2023-05-04 04:20:09 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,21 +3,11 @@
 int main(void)
 {
 	int n;
-	int a[5];
-	int *p;
+int a[5];
+int *p = &a[0]; // initialize pointer to point to the first element of a
 
-	a[2] = 1024;
-	p = &n;
-/*
- * write your line of code here...
- * Remember:
- * - you are not allowed to use a
- * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
- */
-*(p + 7) = 98;
-/* ...so that this prints 98\n */
+a[2] = 1024;
+*(p + 2) = 98; // access third element of a via pointer arithmetic
+
 printf("a[2] = %d\n", a[2]);
-return (0);
 }
